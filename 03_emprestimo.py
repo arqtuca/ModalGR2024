f96064662a0a94a5f7460dc5bc44b0b2de25e5e7
Arthur Rodrigues <121184857+arqtuca@users.noreply.github.com>
2023-11-07 23:24:17 -0300

    Update 03_emprestimo.py

--- 03_emprestimo.py
+++ 03_emprestimo.py
@@ -1,4 +1,13 @@
-# Função para calcular as notas de maior valor
+
+nome = input("Digite seu nome: ")
+data_admissao = input("Digite o ano de sua admissão: ")
+salario_atual = float(input("Digite seu salário atual: "))
+valor_emprestimo = int(input("Digite o valor do empréstimo: "))
+escolha = input("Escolha 'maior' para notas de maior valor ou 'menor' para notas de menor valor: ")
+
+simular_emprestimo(nome, data_admissao, salario_atual, valor_emprestimo, escolha)
+
+# Calcular as notas de maior valor
 def calcular_notas_maior_valor(valor_emprestimo):
     notas_cem = valor_emprestimo // 100
     valor_emprestimo %= 100
@@ -6,7 +15,7 @@ def calcular_notas_maior_valor(valor_emprestimo):
 
     return notas_cem, notas_cinquenta
 
-# Função para calcular as notas de menor valor
+# Calcular as notas de menor valor
 def calcular_notas_menor_valor(valor_emprestimo):
     notas_vinte = valor_emprestimo // 20
     valor_emprestimo %= 20
@@ -17,7 +26,7 @@ def calcular_notas_menor_valor(valor_emprestimo):
 
     return notas_vinte, notas_dez, notas_cinco, notas_dois
 
-# Função para realizar a simulação de empréstimo
+# Simulação de empréstimo
 def simular_emprestimo(nome, data_admissao, salario_atual, valor_emprestimo, escolha):
     anos_de_servico = (2023 - int(data_admissao.split('-')[0]))
     if anos_de_servico > 5 and valor_emprestimo % 2 == 0:
@@ -46,12 +55,5 @@ def simular_emprestimo(nome, data_admissao, salario_atual, valor_emprestimo, esc
     else:
         print("Agradecemos seu interesse, mas você não atende os requisitos mínimos do programa.")
 
-# Solicitar informações do colaborador
-nome = input("Digite seu nome: ")
-data_admissao = input("Digite o ano de sua admissão: ")
-salario_atual = float(input("Digite seu salário atual: "))
-valor_emprestimo = int(input("Digite o valor do empréstimo: "))
-escolha = input("Escolha 'maior' para notas de maior valor ou 'menor' para notas de menor valor: ")
 
-# Realizar a simulação de empréstimo
-simular_emprestimo(nome, data_admissao, salario_atual, valor_emprestimo, escolha)
+
